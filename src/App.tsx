b2e787dfae47fe1b243ca7c2013b711d565ee4f0
Vishkujo <vepicage@gmail.com>
2022-05-20 05:05:14 -0400

    Updating files

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import type { Component } from 'solid-js';
+import { createSignal, createEffect } from 'solid-js';
+import { createStore } from "solid-js/store";
 
 // Highlight a word
 function highlight(word: string) {
   return (
-    <span class="bg-sky-600 p-1 text-white rounded inline-block">
-      {word}
+    <span>
+      <span class="bg-sky-600 p-1 text-white rounded inline-block animate-zoominandout">
+        {word}
+      </span>
+      <span>&nbsp;</span>
     </span>
   )
 }
@@ -14,21 +18,58 @@ type WordProps = {
   text: string;
 }
 
-// Go through all the words and highlight
-const Highlighter = (props: WordProps) => {
+// Create a signal to keep track of which word is highlighted
+const [highlighted, setHighlighted] = createSignal(0);
 
-  // Split the text into an array using a space
-  let words = props.text.split(" ");
+// Create a store which holds a boolean value of whether the effect is paused or not
+const [pauseState, setPause] = createStore(
+  { paused: false }
+);
 
-  return (
-    <div class="text-left px-8 mr-2 mt-5">
-      {words.map((word, index) => {
-        return (
-          highlight(word)
-      )
-      })}
-    </div>
-  )
+// Go through the text and highlight each word
+const Highlighter = ({ text }: WordProps) => {
+  return startHighlighting(text);
+}
+
+function startHighlighting(text: string) { 
+    // Split the text into an array using a space
+    let words = text.split(" ");
+
+    // Create an effect that sets a highlighted word
+    createEffect(() => {
+      // Make an interval to set the highlighted word every 450 milliseconds
+      const interval = setInterval(() => {
+        // Increase the highlighted word index if not paused
+        if (pauseState.paused === false) {
+          // Initialize highlighted word index at 0 then add 1 each time
+          setHighlighted(highlighted => highlighted + 1);
+        }
+        // Once it reaches the end of the word list
+        if (highlighted() === words.length) {
+          // Stop the interval from increasing
+          clearInterval(interval);
+          setHighlighted(-1);
+          setPause({ paused: true });
+        }
+      }, 450);
+    })
+  
+    return (
+      <div class="text-left px-8 mr-2 mt-5">
+        {/* Go through each word in the text */}
+        {words.map((word, index) => {
+          {/* If the word at the current index matches the highlighted one in the signal then...*/ }
+          if (index === highlighted()) {
+            {/* Highlight the word at that index */ }
+            return highlight(words[index]);
+          }
+          else {
+            {/* Otherwise just display the regular word with a space after (since spaces were removed with .split()) */ }
+            return word + " ";
+          }
+        })}
+      </div>
+    )
 }
 
 function App() {
@@ -38,9 +79,9 @@ function App() {
         text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
       />
 
-      <div class="mt-3">
-        <button class="rounded play-button p-2 m-1 border-2 bg-blue-800 text-white border-blue-900">Play</button>
-        <button class="rounded pause-button p-2 m-1 border-2 bg-blue-800 text-white border-blue-900">Pause</button>
+      <div class="mt-3 px-8">
+        <button class="rounded play-button p-2 m-1 border-2 bg-blue-700 hover:bg-blue-900 text-white border-blue-900" onClick={() => setPause({ paused: false })}>Play</button>
+        <button class="rounded pause-button p-2 m-1 border-2 bg-blue-700 hover:bg-blue-900 text-white border-blue-900" onClick={() => setPause({ paused: true })}>Pause</button>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,21 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        zoominandout: {
+          '0%, 100%': {
+            transform: 'scale(1, 1)'
+          },
+          '50%': {
+            transform: 'scale(1.05, 1.05)'
+          },
+        }
+      },
+      animation: {
+        zoominandout: 'zoominandout 1s ease-in-out infinite',
+      }
+    },
   },
   plugins: [],
 }
